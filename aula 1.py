9dbc2f1d8ffd9c0256bfb3b5d4befe586d02568a
logonrmlocal <isa.vdiaz20@gmail.com>
2023-08-07 22:51:49 -0300

    Primeira aula do segundo semestre e setima ao todo

--- aula 1.py
+++ aula 1.py
@@ -10,37 +10,48 @@
 
 #aula dicionario
 
-carros = {"Jeep Renegade":['R$90.000,00', 'ano = 2020'],
-          "Jeep Compass":['R$150.000,00', 'ano = 2020'],
-          "Troller": ['R$200.000,00', 'ano = 2020']}
-print(carros)
-print(carros['Jeep Renegade'])
+# carros = {"Jeep Renegade":['R$90.000,00', 'ano = 2020'],
+#           "Jeep Compass":['R$150.000,00', 'ano = 2020'],
+#           "Troller": ['R$200.000,00', 'ano = 2020']}
+# print(carros)
+# print(carros['Jeep Renegade'])
+#
+# #altera o elemento unico da lista
+# #carros["Jeep Compass"] = 'R$180.000,00'
+#
+# #como possui mais de um elemento devemos definir sua posição:
+# carros["Jeep Compass"][0] = 'R$180.000,00'
+# print(carros)
+# #altera um elemento de varios da lista
+# carros["Jeep Renegade"][1] = "ano =2020"
+# print(carros)
+#
+# #O elemento Troller foi vendido, apague da lista:
+# del carros["Troller"]
+# print(carros)
+#
+# #Adicionar novo elemento:
+#
+# carros["audi"] = ['R$250.000,00', "2023"]
+# print(carros)
+#
+# #Pesquisar se possui ou não um elemento na tabela
+# print("Audi" in carros)
+# print("BMW" in carros)
+#
+# #Imprimir apenas chaves
+# print(carros.keys())
+#
+# #Imprimir apenas valores
+# print(carros.values())
+#
 
-#altera o elemento unico da lista
-#carros["Jeep Compass"] = 'R$180.000,00'
-
-#como possui mais de um elemento devemos definir sua posição:
-carros["Jeep Compass"][0] = 'R$180.000,00'
-print(carros)
-#altera um elemento de varios da lista
-carros["Jeep Renegade"][1] = "ano =2020"
-print(carros)
-
-#O elemento Troller foi vendido, apague da lista:
-del carros["Troller"]
-print(carros)
-
-#Adicionar novo elemento:
-
-carros["audi"] = ['R$250.000,00', "2023"]
-print(carros)
-
-#Pesquisar se possui ou não um elemento na tabela
-print("Audi" in carros)
-print("BMW" in carros)
-
-#Imprimir apenas chaves
-print(carros.keys())
-
-#Imprimir apenas valores
-print(carros.values())
+Mercado = {"maracuja": 2.0, "manga":1.5, "abacate": 3.0}
+while True:
+    Fruta = input("Digite o nome da produto ou fim para terminar:")
+    if Fruta == "fim":
+        break
+    if Fruta in Mercado:
+        print(f"Preço:R${Mercado[Fruta]:.2f}")
+    else:
+        print("Produto não encontrado")
